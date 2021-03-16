035522b7bfe787485ae48ac3f89bb1fca32ec2eb
RUnroe <ryanunroe@gmail.com>
2021-03-16 13:51:49 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const routes = require("./routes/routes");
+
+
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname+"/views");
+app.use(express.static(path.join(__dirname+"/public")));
+
+
+
+app.get("/", routes.index);
+
+app.listen(3000);
